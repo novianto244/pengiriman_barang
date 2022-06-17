d874675554449c8804bad1ad978d317b8f0c99b8
Novianto <novianto.it@jayakencana.com>
2022-06-17 09:41:35 +0700

    17 Juni 2022 Update

--- resources/views/surat_jalan.blade.php
+++ resources/views/surat_jalan.blade.php
@@ -4,6 +4,23 @@
 
 <input type="text" id="nik" value="{{ $data['nik'] }}" class="form-control-sm" hidden readonly>   
 
+<div id="main-container" style="background-color: green; overflow-y: scroll; padding:10px;">
+    <div class="row" style="height: 750px;">
+        <div class="col-md-3" style="background-color: blue;">  
+            PENGIRIMAN BARU
+        </div>
+        <div class="col-md-3" style="background-color: grey;">
+            BERANGKAT
+        </div>
+        <div class="col-md-3" style="background-color: teal;">
+            TERKIRIM
+        </div>
+        <div class="col-md-3" style="background-color: pink;">
+            BATAL
+        </div>
+    </div>
+</div>
+
 
 <script type="text/javascript">
     $(function () {     
@@ -12,6 +29,17 @@
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
+
+        // Set Container Height
+        switch(window.screen.height){
+            case 864:
+                $('#main-container').height(600);
+                break; 
+            default:
+                $('#main-container').height(520);
+                break;
+        }
+        console.log(window.screen.height);
 
        
     });     
