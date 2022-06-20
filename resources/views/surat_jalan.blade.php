6b5c0ed0cb467f8194e9a45c773361f267fb9e11
Novianto <novianto.it@jayakencana.com>
2022-06-20 11:42:54 +0700

    Create model

--- resources/views/surat_jalan.blade.php
+++ resources/views/surat_jalan.blade.php
@@ -29,27 +29,27 @@
     </div>
 
     <div class="row" style="height:1000px; border:solid 1px #d8d8d8; padding:7px 23px 7px 21px; background-color:white;">
-        <div class="col-md-3 bordered-box">  
+        <div class="col-md-3 bordered-box" ondrop="drop(event)" ondragover="allowDrop(event)">  
             <img src="public/img/stage1.png" alt="stage1" class="responsive">
             <div class="row border-bottom dark-background" style="padding-top:15px;">
                 <div class="col-sm-8"> <p class="capital-title-white"><i class="fa fa-clipboard margin-right-min15"></i>PENGIRIMAN BARU</p></div>
                 <div class="col-sm-4 text-right"><p class="badge-total">120000</p></div>
             </div>
             <div class="box-content">
-                isi
+                <p ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true" id="dragtarget1">Isi 1</p>
             </div>
         </div>
-        <div class="col-md-3 bordered-box">  
+        <div class="col-md-3 bordered-box" ondrop="drop(event)" ondragover="allowDrop(event)">  
             <img src="public/img/stage2.png" alt="stage2" class="responsive">
             <div class="row border-bottom dark-background" style="padding-top:15px;">
                 <div class="col-sm-8"> <p class="capital-title-white"><i class="fa fa-car-side margin-right-min15"></i>BERANGKAT</p></div>
                 <div class="col-sm-4 text-right"><p class="badge-total">1200</p></div>
             </div>
             <div class="box-content">
-                isi
+                <p ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true" id="dragtarget2">Isi 2</p>
             </div>
         </div>
-        <div class="col-md-3 bordered-box">  
+        <div class="col-md-3 bordered-box" ondrop="drop(event)" ondragover="allowDrop(event)">  
             <img src="public/img/stage3.png" alt="stage3" class="responsive">
             <div class="row border-bottom dark-background" style="padding-top:15px;">
                 <div class="col-sm-8"> <p class="capital-title-white"><i class="fa fa-clipboard-check margin-right-min15"></i>TERKIRIM</p></div>
@@ -57,17 +57,17 @@
                
             </div>
             <div class="box-content">
-                isi
+                <p ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true" id="dragtarget3">Isi 3</p>
             </div>
         </div>
-        <div class="col-md-3 bordered-box">  
+        <div class="col-md-3 bordered-box" ondrop="drop(event)" ondragover="allowDrop(event)">  
             <img src="public/img/stage4.png" alt="stage4" class="responsive">
             <div class="row border-bottom dark-background" style="padding-top:15px;">
                 <div class="col-sm-8">  <p class="capital-title-white"><i class="fa fa-times-circle margin-right-min15"></i>BATAL</p></div>
                 <div class="col-sm-4 text-right"><p class="badge-total">120</p></div>
             </div>
             <div class="box-content">
-                isi
+                <p ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true" id="dragtarget4">Isi 4</p>
             </div>
         </div>
     </div>
@@ -75,10 +75,26 @@
     <div style="height: 35px; padding:5px; text-align:center; color:gray; font-size:13px;">
         2022 © Jayakencana.com. All rights reserved.
     </div>
+
 </div>
 
 
 <script type="text/javascript">
+    // Dragabel function
+    function dragStart(event) {
+        event.dataTransfer.setData("Text", event.target.id);
+    }
+
+    function allowDrop(event) {
+        event.preventDefault();
+    }
+
+    function drop(event) {
+        event.preventDefault();
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
+
     $(function () {     
         $.ajaxSetup({
             headers: {
@@ -105,6 +121,7 @@
             language: "id",
             weekStart: 0,
         });
+
     });     
 </script>
 
